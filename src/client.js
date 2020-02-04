0eacce6b0dd9c9519e1ade0599fb9f9bbac468a0
Billy Meyers <Billy.Meyers@panerabread.com>
2020-02-04 11:07:15 -0600

    Adds logging and constants to mqtt client

--- src/client.js
+++ src/client.js
@@ -1,5 +1,8 @@
 var mqtt = require('mqtt')
 const MQTT_URL = 'mqtt://mqtt.panerahackathon.com';
+const INGREDIENT_PULLED = 'ingredient/pulled';
+const INGREDIENT_SETUP = 'ingredient/setup';
+const ORDER_RESET = 'order/reset';
 
 const orderManager = require('./OrderManager.js');
 
@@ -9,19 +12,20 @@ const client = mqtt.connect(MQTT_URL)
 
 client.on('connect', function () {
   console.log('connected to', MQTT_URL);
-  client.subscribe('ingredient/pulled');
-  client.subscribe('ingredient/setup');
-  client.subscribe('order/reset');
+  client.subscribe(INGREDIENT_PULLED);
+  client.subscribe(INGREDIENT_SETUP);
+  client.subscribe(ORDER_RESET);
 })
  
 client.on('message', function (topic, message) {
   // message is Buffer
   const messageStr = message.toString();
-  if (topic === 'ingredient/pulled') {
+  console.log('MESSAGE received', topic, messageStr);
+  if (topic === INGREDIENT_PULLED) {
     orderManager.addIngredient(JSON.parse(messageStr));
-  } else if (topic === 'ingredient/setup') {
+  } else if (topic === INGREDIENT_SETUP) {
     orderManager.updateIngredientToContainerMapping(JSON.parse(messageStr));
-  } else if (topic === 'order/reset') {
+  } else if (topic === ORDER_RESET) {
     orderManager.resetOrder();
   }
 })
